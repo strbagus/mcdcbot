0555dd3c4121f72f6502f3db98ff64bf8b7d2646
Satrio Bagus <satrio.baguswbw@gmail.com>
2025-03-06 13:17:54 +0700

    fix: discord styling output log

--- utils/shell.go
+++ utils/shell.go
@@ -51,10 +51,25 @@ func LogListen(ctx context.Context, s *discordgo.Session, m *discordgo.MessageCr
 			return
 		default:
 			line := scanner.Text()
-            fmt.Println("server: ", line)
+			fmt.Println("server: ", line)
 			if strings.Contains(line, "#msg") || strings.Contains(line, "joined the game") || strings.Contains(line, "left the game") {
-	            msg := strings.Split(line, ":")
-		        s.ChannelMessageSend(m.ChannelID, msg[len(msg)-1])
+				msg := strings.Split(line, ":")
+				tmp := msg[len(msg)-1]
+				var res string
+				if strings.Contains(line, "joined the game") {
+					name := strings.TrimSuffix(tmp, " joined the game")
+					res = fmt.Sprintf("%s: **joined the game**", name)
+				} else if strings.Contains(line, "left the game") {
+					name := strings.TrimSuffix(tmp, " left the game")
+					res = fmt.Sprintf("%s: **left the game**", name)
+				} else {
+					start := strings.Index(tmp, "<") + 1
+					end := strings.Index(tmp, ">")
+					username := tmp[start:end]
+					message := strings.TrimPrefix(tmp[end+2:], "#msg ")
+					res = fmt.Sprintf("%s: *%s*", username, message)
+				}
+				s.ChannelMessageSend(m.ChannelID, res)
 			}
 		}
 	}
